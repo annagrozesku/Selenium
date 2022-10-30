b600c492d2838c40820761776b15dcfa87a85d85
Анна Грозеску <agy96@yandex.ru>
2022-10-30 19:22:36 +0300

    Add AppVeyor, change test

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -1,16 +1,19 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderCardTest {
-   private WebDriver driver;
+     private WebDriver driver;
 
    @BeforeAll
     static void setupAll() {
@@ -20,7 +23,9 @@ public class OrderCardTest {
    @BeforeEach
     void setup() {
        ChromeOptions options = new ChromeOptions();
-       options.setHeadless(true);
+      options.addArguments("--disable-dev-shm-usage");
+      options.addArguments("--no-sandbox");
+      options.addArguments("--headless");
        driver = new ChromeDriver(options);
    }
 
@@ -30,7 +35,18 @@ public class OrderCardTest {
        driver = null;
    }
 
-   
+   @Test
+   public void shouldMustBeSubmitted (){
+      driver.get("http://localhost:9999/");
+      driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Пупкин Василий");
+      driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79995687523");
+      driver.findElement(By.className("checkbox__box")).click();
+      driver.findElement(By.className("button__text")).click();
+      String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+      String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+      assertEquals(expected, actual);
+
+   }
 
 
 }
